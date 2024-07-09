91e0839029f165a9d9aa3ba9e01331dcb6ed7f3c
Ullrich Praetz <ullrich@praetz.de>
2024-07-09 19:32:27 +0200

    add benchmark: CreateWorld

---
+++ Arch/CreateWorld.cs
@@ -0,0 +1,22 @@
+﻿using Arch.Core;
+using BenchmarkDotNet.Attributes;
+
+namespace Arch;
+
+[ShortRunJob]
+public class CreateWorld
+{
+    [GlobalSetup]
+    public void Setup() { }
+    
+    [GlobalCleanup]
+    public void Shutdown() { }
+    
+    [Benchmark]
+    [BenchmarkCategory(Categories.Arch)]
+    public void Run()
+    {
+        var world = World.Create();
+        World.Destroy(world);
+    }
+}
---
+++ Friflo.Engine.ECS/CreateWorld.cs
@@ -0,0 +1,23 @@
+﻿using Arch.Core;
+using BenchmarkDotNet.Attributes;
+
+namespace Friflo.Engine.ECS;
+
+[ShortRunJob]
+public class CreateWorld
+{
+    [GlobalSetup]
+    public void Setup() { }
+    
+    [GlobalCleanup]
+    public void Shutdown() {
+    }
+    
+    [Benchmark]
+    [BenchmarkCategory(Categories.Arch)]
+    public void Run()
+    {
+        _ = new EntityStore();
+        // nothing to Dispose() - has no unmanaged resources
+    }
+}
