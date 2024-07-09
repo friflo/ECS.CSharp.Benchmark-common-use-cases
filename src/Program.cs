4807584e755a07f88c519c42564c4b273d59a5ff
Ullrich Praetz <ullrich@praetz.de>
2024-07-09 22:23:56 +0200

    changed Artifacts folder

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Reflection;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
 Console.WriteLine("Hello, World!");
 
+ManualConfig customConfig = DefaultConfig.Instance.WithArtifactsPath(@"../Artifacts");
 BenchmarkSwitcher
     .FromAssembly(Assembly.GetExecutingAssembly())
-    .Run(args);
+    .Run(args, customConfig);
