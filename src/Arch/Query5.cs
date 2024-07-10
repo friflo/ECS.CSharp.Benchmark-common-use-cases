144abfd500ebed67c3e8ec69683b828c3839cf3c
Ullrich Praetz <ullrich@praetz.de>
2024-07-10 02:19:52 +0200

    add benchmark: Query5

---
+++ src/Arch/Query5.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.CompilerServices;
+using Arch.Core;
+using Arch.Core.Extensions;
+using Arch.Types;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+
+namespace Arch;
+
+[ShortRunJob]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByMethod)]
+public class Query5
+{
+    private World   world;
+    private Query   query;
+    
+    [Params(Constant.EntityCountP1)]
+    public int EntityCount { get; set; }
+    
+    [GlobalSetup]
+    public void Setup() {
+        world   = World.Create();
+        for (int n = 0; n < EntityCount; n++) {
+            var entity = world.Create();
+            entity.Add(new Component1());
+            entity.Add(new Component2());
+            entity.Add(new Component3());
+            entity.Add(new Component4());
+            entity.Add(new Component5());
+        }
+        var queryDescription = new QueryDescription().WithAll<Component1,Component2,Component3,Component4,Component5>();
+        query = world.Query(in queryDescription);
+        Assert.AreEqual(EntityCount, world.CountEntities(queryDescription));
+    }
+    
+    [GlobalCleanup]
+    public void Shutdown() {
+        World.Destroy(world);
+    }
+    
+    // [Benchmark]
+    public void Run()
+    {
+        foreach(ref var chunk in query.GetChunkIterator())
+        {
+            /* ???
+            var components1 = chunk.GetFirst<Component1>;
+            var components2 = chunk.GetFirst<Component2>;
+            var components3 = chunk.GetFirst<Component3>;
+            var components4 = chunk.GetFirst<Component4>;
+            var components5 = chunk.GetFirst<Component5>;
+            foreach(var entity in chunk)                    // Iterate over each row/entity inside chunk
+            {
+                ref Component1 c1 = ref Unsafe.Add(ref components1, entity);
+                ref var c2 = ref Unsafe.Add(ref components2, entity);
+                ref var c3 = ref Unsafe.Add(ref components3, entity);
+                ref var c4 = ref Unsafe.Add(ref components4, entity);
+                ref var c5 = ref Unsafe.Add(ref components5, entity);
+                c1.value = c2.value;
+            }
+            */
+        }
+    }
+}
--- src/Friflo.Engine.ECS/Query5.cs
+++ src/Friflo.Engine.ECS/Query5.cs
@@ -25,20 +25,21 @@ public class Query5
             entity.AddComponent<Component5>();
         }
         query = world.Query<Component1,Component2,Component3,Component4,Component5>();
+        Assert.AreEqual(EntityCount, query.Count);
     }
     
-    // [Benchmark]
-    public void Run() {
-        foreach (var (components1, components2, components3, components4, components5, entities) in query.Chunks) {
+    [Benchmark]
+    public void Run()
+    {
+        foreach (var (components1, components2, components3, components4, components5, _) in query.Chunks) {
             var span1 = components1.Span;
             var span2 = components2.Span;
             var span3 = components3.Span;
             var span4 = components4.Span;
             var span5 = components5.Span;
-            for (int n = 0; n < entities.Length; n++) {
+            for (int n = 0; n < components1.Length; n++) {
                 span1[n].value = span2[n].value + span3[n].value + span4[n].value * span5[n].value;  
             }
-            
         }
     }
 }
