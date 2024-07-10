17ba557b4d1386d819c004fd2fcc75c9a3cadc3c
Ullrich Praetz <ullrich@praetz.de>
2024-07-10 04:30:23 +0200

    add BenchUtils

---
+++ src/Arch/Types/BenchUtils.cs
@@ -0,0 +1,29 @@
+﻿using Arch.Core;
+using Arch.Core.Extensions;
+
+namespace Arch.Types;
+
+public static class BenchUtils
+{
+    public static Entity[] CreateEntities (World world, int count)
+    {
+        var entities = new Entity[count];
+        for (int n = 0; n < count; n++) {
+            entities[n] = world.Create();
+        }
+        return entities;
+    }
+    
+    public static Entity[] AddComponents(Entity[] entities)
+    {
+        foreach (var entity in entities)
+        {
+            entity.Add<Component1>();
+            entity.Add<Component2>();
+            entity.Add<Component3>();
+            entity.Add<Component4>();
+            entity.Add<Component5>();
+        }
+        return entities;
+    }
+}
---
+++ src/Friflo.Engine.ECS/Types/BenchUtils.cs
@@ -0,0 +1,27 @@
+﻿namespace Friflo.Engine.ECS.Types;
+
+public static class BenchUtils
+{
+    
+    public static Entity[] CreateEntities (EntityStore world, int count)
+    {
+        var entities = new Entity[count];
+        for (int n = 0; n < count; n++) {
+            entities[n] = world.CreateEntity();
+        }
+        return entities;
+    }
+    
+    public static Entity[] AddComponents(Entity[] entities)
+    {
+        foreach (var entity in entities)
+        {
+            entity.AddComponent<Component1>();
+            entity.AddComponent<Component2>();
+            entity.AddComponent<Component3>();
+            entity.AddComponent<Component4>();
+            entity.AddComponent<Component5>();
+        }
+        return entities;
+    }
+}
