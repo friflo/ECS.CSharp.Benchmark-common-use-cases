0fb7fe7551ff72d26c75ce5075ceadb6bc50d812
Ullrich Praetz <ullrich@praetz.de>
2024-07-13 12:48:01 +0200

    Flecs.NET - add command buffer benchmark

---
+++ src/Flecs.NET/CommandBufferAddRemoveT2.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using Flecs.NET.Core;
+
+namespace Flecs.NET;
+
+[ShortRunJob]
+[BenchmarkCategory(Category.CommandBufferAddRemoveT2)]
+// ReSharper disable once InconsistentNaming
+public class CommandBufferAddRemoveT2_FlecsNet
+{
+    private World       world;
+    private Entity[]    entities;
+    
+    [GlobalSetup]
+    public void Setup() {
+        world       = World.Create();
+        entities    = world.CreateEntities(Constants.EntityCount);
+    }
+    
+    [GlobalCleanup]
+    public void Shutdown() {
+        world.Dispose();
+    }
+
+    [Benchmark]
+    public void Run()
+    {
+        world.DeferBegin();
+        foreach (var entity in entities) {
+            entity
+                .Add<Component1>()
+                .Add<Component2>();
+        }
+        world.DeferEnd();
+
+        world.DeferBegin();
+        foreach (var entity in entities) {
+            entity
+                .Remove<Component1>()
+                .Remove<Component2>();
+        }
+        world.DeferEnd();
+    }
+}
