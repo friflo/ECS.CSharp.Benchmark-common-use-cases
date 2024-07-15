ded1ef5368b8d1fff8b0a5606792d483026bf2f8
Ullrich Praetz <ullrich@praetz.de>
2024-07-15 17:29:12 +0200

    add benchmark: TinyEcs/AddRemoveRelations

---
+++ src/TinyEcs/AddRemoveRelations.cs
@@ -0,0 +1,72 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace TinyEcs;
+
+[ShortRunJob]
+[BenchmarkCategory(Category.AddRemoveRelations)]
+// ReSharper disable once InconsistentNaming
+public class AddRemoveRelations_TinyEcs
+{
+    private World           world;
+    private EntityView[]    entities;
+    [Params(Constants.RelationCountP1, Constants.RelationCountP2)]
+    public  int             RelationCount { get; set; }
+    
+    [GlobalSetup]
+    public void Setup() {
+        world       = new World();
+        entities    = world.CreateEntities(Constants.EntityCount);
+    }
+    
+    [GlobalCleanup]
+    public void Shutdown() {
+        world.Dispose();
+    }
+
+    [Benchmark]
+    public void Run()
+    {
+        if (RelationCount == 1) {
+            AddRemove1Relation();
+            return;
+        }
+        AddRemove10Relations();
+    }
+    
+    private void AddRemove1Relation()
+    {
+        foreach (var entity in entities)
+        {
+            entity.Set<Tag1, Relation>(new Relation(1337));
+            entity.Unset<Tag1, Relation>();
+        }
+    }
+    
+    private void AddRemove10Relations()
+    {
+        foreach (var entity in entities)
+        {
+            entity.Set<Tag1,  Relation>(new Relation(1337));
+            entity.Set<Tag2,  Relation>(new Relation(1337));
+            entity.Set<Tag3,  Relation>(new Relation(1337));
+            entity.Set<Tag4,  Relation>(new Relation(1337));
+            entity.Set<Tag5,  Relation>(new Relation(1337));
+            entity.Set<Tag6,  Relation>(new Relation(1337));
+            entity.Set<Tag7,  Relation>(new Relation(1337));
+            entity.Set<Tag8,  Relation>(new Relation(1337));
+            entity.Set<Tag9,  Relation>(new Relation(1337));
+            entity.Set<Tag10, Relation>(new Relation(1337));
+
+            entity.Unset<Tag1,  Relation>();
+            entity.Unset<Tag2,  Relation>();
+            entity.Unset<Tag3,  Relation>();
+            entity.Unset<Tag4,  Relation>();
+            entity.Unset<Tag5,  Relation>();
+            entity.Unset<Tag6,  Relation>();
+            entity.Unset<Tag7,  Relation>();
+            entity.Unset<Tag8,  Relation>();
+            entity.Unset<Tag9,  Relation>();
+            entity.Unset<Tag10, Relation>();
+        }
+    }
+}
--- src/TinyEcs/_Components.cs
+++ src/TinyEcs/_Components.cs
@@ -1,4 +1,5 @@
-﻿namespace TinyEcs;
+﻿// ReSharper disable NotAccessedPositionalProperty.Global
+namespace TinyEcs;
 
 #pragma warning disable CS0649 // Field is never assigned to, and will always have its default value
 
@@ -14,3 +15,17 @@ public record struct Component5(int Value);
 
 // ReSharper disable once NotAccessedPositionalProperty.Global
 internal record struct LinkRelation;
+
+internal record struct Tag1;
+internal record struct Tag2;
+internal record struct Tag3;
+internal record struct Tag4;
+internal record struct Tag5;
+internal record struct Tag6;
+internal record struct Tag7;
+internal record struct Tag8;
+internal record struct Tag9;
+internal record struct Tag10;
+
+// --- TinyEcs specific: discriminator for relations are tags
+internal record struct Relation(int Value);
