59b7e9609358304fe19eefb7d0424ceb6af9410c
Ullrich Praetz <ullrich@praetz.de>
2024-07-15 13:58:00 +0200

    add benchmark: TinyEcs/ComponentEvents

---
+++ src/TinyEcs/ComponentEvents.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+
+// ReSharper disable NotAccessedField.Local
+namespace TinyEcs;
+
+[ShortRunJob]
+[BenchmarkCategory(Category.ComponentEvents)]
+// ReSharper disable once InconsistentNaming
+public class ComponentEvents_TinyEcs
+{
+    private World           world;
+    private EntityView[]    entities;
+    private int             iterations;
+    private int             added;
+    private int             removed;
+    
+    [GlobalSetup]
+    public void Setup() {
+        world       = new World();
+        entities    = world.CreateEntities(Constants.EventCount);
+        world.OnComponentSet += (_, _) => { added++;   };
+        world.OnComponentSet += (_, _) => { removed++; };
+    }
+    
+    [GlobalCleanup]
+    public void Shutdown() {
+        world.Dispose();
+        // var expect = iterations * Constants.EventCount;
+        // Assert.AreEqual(expect, added);     Exception - expect: 29491300, was: 29491301
+        // Assert.AreEqual(expect, removed);   Exception - expect: 29491300, was: 29491301
+    }
+
+    [Benchmark]
+    public void Run()
+    {
+        iterations++;
+        foreach (var entity in entities) {
+            entity.Set(new Component1());
+            entity.Unset<Component1>();
+        }
+    }
+}
