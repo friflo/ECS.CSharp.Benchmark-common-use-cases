c644018e96ecbae0515cc6131a86a761563a2fc5
Ullrich Praetz <ullrich@praetz.de>
2024-07-15 18:03:59 +0200

    add benchmark: Fennecs/AddRemoveRelations

---
+++ src/Fennecs/AddRemoveRelations.cs
@@ -0,0 +1,57 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace fennecs;
+
+[ShortRunJob]
+[BenchmarkCategory(Category.AddRemoveRelations)]
+// ReSharper disable once InconsistentNaming
+public class AddRemoveRelations_Fennecs
+{
+    private World       world;
+    private Entity[]    entities;
+    [Params(Constants.RelationCountP1, Constants.RelationCountP2)]
+    public  int         RelationCount { get; set; }
+    
+    [GlobalSetup]
+    public void Setup() {
+        world       = new World();
+        entities    = world.CreateEntities(Constants.EntityCount);
+    }
+    
+    [GlobalCleanup]
+    public void Shutdown() {
+        world.Dispose();
+    }
+
+    [Benchmark]
+    public void Run()
+    {
+        if (RelationCount == 1) {
+            AddRemove1Relation();
+            return;
+        }
+        AddRemove10Relations();
+    }
+    
+    private void AddRemove1Relation()
+    {
+        foreach (var entity in entities)
+        {
+            entity.Add   (Link.With(RelationKey.Key1));
+            entity.Remove(Link.With(RelationKey.Key1));
+        }
+    }
+    
+    private void AddRemove10Relations()
+    {
+        foreach (var entity in entities)
+        {
+            foreach (var key in RelationKey.Keys) {
+                entity.Add   (Link.With(key));
+            }
+            foreach (var key in RelationKey.Keys) {
+                entity.Remove(Link.With(key));
+            }
+        }
+    }
+}
--- src/Fennecs/_Components.cs
+++ src/Fennecs/_Components.cs
@@ -14,3 +14,19 @@ internal record struct Component5(int Value);
 
 // ReSharper disable once NotAccessedPositionalProperty.Global
 internal record struct LinkRelation(int Value);
+
+internal static class RelationKey
+{
+    public const string Key1 = nameof(Key1);
+    public const string Key2 = nameof(Key2);
+    public const string Key3 = nameof(Key3);
+    public const string Key4 = nameof(Key4);
+    public const string Key5 = nameof(Key5);
+    public const string Key6 = nameof(Key6);
+    public const string Key7 = nameof(Key7);
+    public const string Key8 = nameof(Key8);
+    public const string Key9 = nameof(Key9);
+    public const string Key10= nameof(Key10);
+    
+    public static readonly string[] Keys = new [] { Key1, Key2, Key3, Key4, Key5, Key6, Key7, Key8, Key9, Key10 };
+}
