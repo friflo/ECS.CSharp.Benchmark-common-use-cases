18515ddb0227df9f6ee841378aa0bb9c11b0973a
Ullrich Praetz <ullrich@praetz.de>
2024-07-20 14:22:42 +0200

    TinyEcs: added benchmark - ChildEntitiesAddRemove

---
+++ src/TinyEcs/ChildEntitiesAddRemove.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace TinyEcs;
+
+[BenchmarkCategory(Category.ChildEntitiesAddRemove)]
+// ReSharper disable once InconsistentNaming
+public class ChildEntitiesAddRemove_TinyEcs
+{
+    private World           world;
+    private EntityView[]    parents;
+    private EntityView[][]  children;
+    private int             entityCount;
+    private int             childCount;
+    
+    [GlobalSetup]
+    public void Setup() {
+        world       = new World();
+        entityCount = Constants.EntityCount;
+        childCount  = Constants.ChildCount;
+        parents     = world.CreateEntities(entityCount).AddComponents();
+        children    = new EntityView[entityCount][];
+        for (int n = 0; n < entityCount; n++) {
+            children[n] = world.CreateEntities(childCount).AddComponents();
+        }
+    }
+    
+    [GlobalCleanup]
+    public void Shutdown() {
+        world.Dispose();
+    }
+
+    [Benchmark]
+    public void Run()
+    {
+        for (int n = 0; n < entityCount; n++) {
+            for (int child = 0; child < childCount; child++) {
+                parents[n].AddChild(children[n][child]);
+            }
+        }
+        // Assert.AreEqual(entityCount * childCount, CountChildren());
+        for (int n = 0; n < entityCount; n++) {
+            for (int child = childCount - 1; child >= 0; child--) {
+                children[n][child].Unset<Defaults.ChildOf>(parents[n]);
+            }
+        }
+    }
+    
+    // method only for verification
+    private int CountChildren()
+    {
+        int count = 0;
+        for (int n = 0; n < entityCount; n++) {
+            for (int child = 0; child < childCount; child++) {
+                var hasParent = children[n][child].Has<Defaults.ChildOf>(parents[n]);
+                count += hasParent ? 1 : 0;
+            }
+        }
+        return count;
+    }
+}
