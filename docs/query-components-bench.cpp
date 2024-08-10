de7924c98dacf47ffb26052a405a7df1473d6ea1
friflo <ullrich@praetz.de>
2024-08-10 12:49:54 +0200

    add c++ cli cmd

--- docs/query-components-bench.cpp
+++ docs/query-components-bench.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <chrono>
 
-// Windows > cl /O2 query-components-bench.cpp
+// Windows > cl  /O2 query-components-bench.cpp
+// macOS   > g++ /O2 query-components-bench.cpp -o bench
 
 void execute(int& c1, int& c2, int& c3, int& c4, int& c5)
 {
@@ -23,19 +24,24 @@ int main()
     int count  = 100000;
     int repeat = 10000;
 
-    auto arr1 = new int[count];
-    auto arr2 = new int[count];
-    auto arr3 = new int[count];
-    auto arr4 = new int[count];
-    auto arr5 = new int[count];
+    int* arr1 = new int[count];
+    int* arr2 = new int[count];
+    int* arr3 = new int[count];
+    int* arr4 = new int[count];
+    int* arr5 = new int[count];
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
     benchmark(arr1, arr2, arr3, arr4, arr5, count, repeat);
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() / repeat;
+    long duration = std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() / repeat;
 
     std::cout << "entities: " << count << " duration: " << duration << " ns";
+
+    int dummy = 0;
+    for (int n = 0; n < count; n++)
+        dummy += arr1[n] + arr2[n] + arr3[n] + arr4[n] + arr5[n];    
+    std::cout << "dummy: " << dummy;
 }
 
