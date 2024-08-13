b32ed933b18f1511f7d543d4b5e8d3ae0ae3dad9
Lars <behethangames@gmail.com>
2024-08-13 02:14:31 +0200

    Removed method.

--- src/Arch/QueryComponents.cs
+++ src/Arch/QueryComponents.cs
@@ -10,7 +10,6 @@ public class QueryComponents_Arch : QueryComponents
     private World               world;
     private QueryDescription    query1Description;
     private QueryDescription    query5Description;
-    private Query query1;
     private ForEach1            forEach1;
     private ForEach5            forEach5;
 
@@ -21,8 +20,6 @@ public class QueryComponents_Arch : QueryComponents
         world.CreateEntities(Entities).AddComponents();
         query1Description = new QueryDescription().WithAll<Component1>();
         query5Description = new QueryDescription().WithAll<Component1,Component2,Component3,Component4,Component5>();
-
-        query1 = world.Query(query1Description);
         Check.AreEqual(Entities, world.CountEntities(query5Description));
     }
 
@@ -35,18 +32,6 @@ public class QueryComponents_Arch : QueryComponents
     protected override void Run1Component()
     {
         world.InlineQuery<ForEach1, Component1>(query1Description, ref forEach1);
-    }
-
-    protected void Run1Component_Query()
-    {
-        foreach(ref var chunk in query1.GetChunkIterator())
-        {
-            ref var array = ref chunk.GetFirst<Component1>();
-            foreach (var entity in chunk)
-            {
-                Unsafe.Add(ref array, entity).Value++;
-            }
-        }
     }
 
     protected override void Run5Components()
