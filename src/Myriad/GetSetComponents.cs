b63b9ba46c2440c0908eea0818d61446652a6de0
Martin Evans <martindevans@gmail.com>
2025-07-09 13:56:41 +0100

    - Update Myriad.ECS to v36.6  - Using new reftuple to get all 5 components at once in `GetSetComponents` benchmark

--- src/Myriad/GetSetComponents.cs
+++ src/Myriad/GetSetComponents.cs
@@ -33,12 +33,16 @@ public class GetSetComponents_Myriad : GetSetComponents
 
     protected override void Run5Components()
     {
-        foreach (var entity in entities) {
-            entity.GetComponentRef<Component1>() = new Component1();
-            entity.GetComponentRef<Component2>() = new Component2();
-            entity.GetComponentRef<Component3>() = new Component3();
-            entity.GetComponentRef<Component4>() = new Component4();
-            entity.GetComponentRef<Component5>() = new Component5();
+        foreach (var entity in entities)
+        {
+            // Get references to all 5 components in one pass
+            var (_, c1, c2, c3, c4, c5) = entity.GetComponentRef<Component1, Component2, Component3, Component4, Component5>();
+
+            c1.Ref = new Component1();
+            c2.Ref = new Component2();
+            c3.Ref = new Component3();
+            c4.Ref = new Component4();
+            c5.Ref = new Component5();
         }
     }
 }
